2e2a478f5e2ef06db0d1beb7b205a40556596c2c
Erlend Bergeng <Erlend@lab.it.kvn.no>
2026-09-21 15:27:39 +0200

    login shell

---
+++ innlogging.py
@@ -0,0 +1,56 @@
+
+def vis_startmeny():
+    print("\n=== STARTMENY ===")
+    print("1) Logg inn")
+    print("2) Registrer ny bruker")
+    print("3) Avslutt")
+    valg = input("Hva ønsker du å gjøre? ")
+
+    if valg == "1":
+        # logikk for innloggging
+        print("du er logget inn")
+        return "innlogget"
+
+    elif valg == "2":
+        #logikk for registrere ny bruker
+        return "start"
+
+    elif valg == "3":
+        return "quit"
+
+    else:
+        print("ugyldig valg, velg ett nymmer fra menyen")
+        return "start"
+
+
+def vis_innlogget_meny():
+    print("\n=== MENY(innlogget) ===")
+    print("1) fortell en random vits")
+    print("2) Logg ut")
+    print("3) avslutt")
+    valg = input("Hva ønsker du å gjøre? ")
+
+    if valg == "1":
+        #logikk for å fortelle en random vits
+        return "innlogget"
+
+    elif valg == "2":
+        print("du er nå logget ut....")
+        return "start"
+
+    elif valg == "3":
+        return "quit"
+
+    else:
+        print("ugyldig valg, velg ett nymmer fra menyen")
+        return "innlogget"
+
+def main():
+    state = "start"
+    while state != "quit":
+        if state == "start":
+            state = vis_startmeny()
+        elif state == "innlogget":
+            state = vis_innlogget_meny()
+
+main()
